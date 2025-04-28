e2d5ced480e5c18a5be456d6ec65591fc6e5b620
Felipe Barcellos <felipe.cellos1@gmail.com>
2025-04-28 04:43:19 -0300

    Updated main

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from modulos.print import PrintContacts
+from modulos.gemini import run
+from modulos.confirm import Confirm
 
 if __name__ == "__main__":
     PrintContacts().print()
+    status = Confirm("Print Contatos").status
+    if status == "continue":
+        run()
